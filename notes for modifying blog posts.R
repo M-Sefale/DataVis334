dfa63b3a117185947af3747575e9e7c0bbb630af
Lala Sefale <sefale2001@gmail.com>
2026-02-25 13:38:28 -0500

    Adding descriptive titles to Blog page and github link

---
+++ notes for modifying blog posts.R
@@ -0,0 +1,15 @@
+notes to modify:
+  
+  In quarto.yml
+*change title
+*change outside links to your github and your twitter
+* change theme (default is cosmo)
+
+In about
+
+* change links
+*change title
+* can change picture if you want
+* add desciption (change "about this blog")
+
+change post titles
